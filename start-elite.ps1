844b22ff9552a14d8b3cea51d7d5aa4387c7c39a
Kage <2138096+Duvrazh@users.noreply.github.com>
2022-11-22 22:14:06 -0700

    Formatting, logic, user settings section

--- start-elite.ps1
+++ start-elite.ps1
@@ -1,24 +1,80 @@
+#### User Configuration ####
+
+<# For each of the following programs, set them to true or false to determine if the script will attempt to launch them or not.
+# The script will automatically ignore lines for software that does not exist on your machine. #>
+
+$EDHM_UI = $true
+$VoiceAttack = $true
+$EDMarketConnector = $true
+$EDDiscovery = $true
+$EDEngineer = $true
+$EliteObservatory = $true
+$EliteTrack = $false
+$EliteDangerous = $true
+
+#### Do Not Edit Beyond This Line ####
+
 ### Local ###
+
 # EDHM_UI
-if (!(Get-Process -Name EDHM*)) { Start-Process -WindowStyle Maximized -ErrorAction Continue -FilePath 'C:\Users\Kyle\AppData\Local\EDHM_UI\EDHM_UI_mk2.exe' }
+if ($EDHM_UI) {
+    if (!(Get-Process -Name EDHM* -ErrorAction SilentlyContinue)) { 
+        Start-Process -WindowStyle Maximized -ErrorAction SilentlyContinue -FilePath 'C:\Users\Kyle\AppData\Local\EDHM_UI\EDHM_UI_mk2.exe' 
+    } 
+}
+
 # VoiceAttack
-if (Get-Process -Name VoiceAttack) { $true } else { Start-Process -WindowStyle Maximized -ErrorAction Continue -FilePath "C:\Program Files\VoiceAttack\VoiceAttack.exe" }
+if ($VoiceAttack) {
+    if (!(Get-Process -Name VoiceAttack -ErrorAction SilentlyContinue)) {
+        Start-Process -WindowStyle Maximized -ErrorAction SilentlyContinue -FilePath 'C:\Program Files\VoiceAttack\VoiceAttack.exe' 
+    } 
+}
 
 ### Community Data ###
-# ED Market Connector
-if (!(Get-Process -Name EDMarketConnector)) { Start-Process -WindowStyle Maximized -ErrorAction Continue -FilePath 'C:\Program Files (x86)\EDMarketConnector\EDMarketConnector.exe' }
-# ED Discovery
-if (!(Get-Process -Name EDDiscovery)) { Start-Process -WindowStyle Maximized -ErrorAction Continue -FilePath 'C:\Program Files\EDDiscovery\EDDiscovery.exe' }
-# ED Engineer
-if (!(Get-Process -Name EDEngineer)) { Start-Process -WindowStyle Maximized -ErrorAction Continue -FilePath 'C:\Users\Kyle\AppData\Local\Apps\2.0\D96VD2JJ.7JH\585QYJEO.5TD\eden..tion_b9c6c2d0b4f2eae5_0001.0001_37a5eebcaa7d7023\EDEngineer.exe' }
-# Elite Observatory
-if (!(Get-Process -Name ObservatoryCore)) { Start-Process -WindowStyle Maximized -ErrorAction Continue -FilePath 'C:\Program Files\Elite Observatory\ObservatoryCore.exe' }
 
-### Twitch ###
+# ED Market Connector
+if ($EDMarketConnector) {
+    if (!(Get-Process -Name EDMarketConnector -ErrorAction SilentlyContinue)) {
+        Start-Process -WindowStyle Maximized -ErrorAction SilentlyContinue -FilePath 'C:\Program Files (x86)\EDMarketConnector\EDMarketConnector.exe' 
+    } 
+}
+
+# ED Discovery
+if ($EDDiscovery) {
+    if (!(Get-Process -Name EDDiscovery -ErrorAction SilentlyContinue)) {
+        Start-Process -WindowStyle Maximized -ErrorAction SilentlyContinue -FilePath 'C:\Program Files\EDDiscovery\EDDiscovery.exe' 
+    } 
+}
+
+# ED Engineer
+if ($EDEngineer) {
+    if (!(Get-Process -Name EDEngineer -ErrorAction SilentlyContinue)) {
+        Start-Process -WindowStyle Maximized -ErrorAction SilentlyContinue -FilePath 'C:\Users\Kyle\AppData\Local\Apps\2.0\D96VD2JJ.7JH\585QYJEO.5TD\eden..tion_b9c6c2d0b4f2eae5_0001.0001_37a5eebcaa7d7023\EDEngineer.exe' 
+    }
+}
+
+# Elite Observatory
+if ($EliteObservatory) {
+    if (!(Get-Process -Name ObservatoryCore -ErrorAction SilentlyContinue)) {
+        tart-Process -WindowStyle Maximized -ErrorAction SilentlyContinue -FilePath 'C:\Program Files\Elite Observatory\ObservatoryCore.exe' 
+    }
+}
+
+### Streaming ###
+
 # EliteTrack
-if (!(Get-Process -Name EDHM*)) { Start-Process -WindowStyle Maximized -ErrorAction Continue -FilePath 'C:\Users\Kyle\AppData\Local\Programs\EliteTrack\EliteTrack.exe' }
+if ($EliteTrack) {
+    if (!(Get-Process -Name EliteTrack* -ErrorAction SilentlyContinue)) {
+        Start-Process -WindowStyle Maximized -ErrorAction SilentlyContinue -FilePath 'C:\Users\Kyle\AppData\Local\Programs\EliteTrack\EliteTrack.exe' 
+    }
+}
 
 ### Elite Dangerous ###
-if (!(Get-Process -Name EliteDangerous64 )) { Start-Process -WindowStyle Maximized -ErrorAction Continue -FilePath 'steam://rungameid/359320' }
+
+if ($EliteDangerous) {
+    if (!(Get-Process -Name EliteDangerous64 -ErrorAction SilentlyContinue)) {
+        Start-Process -WindowStyle Maximized -ErrorAction SilentlyContinue -FilePath 'steam://rungameid/359320' 
+    }
+}
 
 Exit
