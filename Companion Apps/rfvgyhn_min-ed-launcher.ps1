2bbbaf080f182dadeb6ca0f09d6a5b2d2a32deec
gitkagehub <khgreenprime@gmail.com>
2025-08-16 21:16:36 -0700

    min-ed-launcher

--- Companion Apps/rfvgyhn_min-ed-launcher.ps1
+++ Companion Apps/rfvgyhn_min-ed-launcher.ps1
@@ -1,9 +1,12 @@
-$repo = "rfvgyhn/min-ed-launcher"
+### User Config ###
+$elitePath = 'G:\SteamLibrary\steamapps\common\Elite Dangerous'
 $filenamePattern = "*win-x64.zip"
-$pathExtract = "G:\EliteApps\min-ed-launcher"
 $innerDirectory = $true
+$pathExtract = "G:\EliteApps\min-ed-launcher"
 $preRelease = $false
+$repo = "rfvgyhn/min-ed-launcher"
 
+# Ask GitHub for the target release and download to temp
 if ($preRelease) {
     $releasesUri = "https://api.github.com/repos/$repo/releases"
     $downloadUri = ((Invoke-RestMethod -Method GET -Uri $releasesUri)[0].assets | Where-Object name -like $filenamePattern ).browser_download_url
@@ -13,21 +16,26 @@ else {
     $downloadUri = ((Invoke-RestMethod -Method GET -Uri $releasesUri).assets | Where-Object name -like $filenamePattern ).browser_download_url
 }
 
+# Extract the release
 $pathZip = Join-Path -Path $([System.IO.Path]::GetTempPath()) -ChildPath $(Split-Path -Path $downloadUri -Leaf)
-
 Invoke-WebRequest -Uri $downloadUri -Out $pathZip
 
+# Clear extraction container
 Remove-Item -Path $pathExtract -Recurse -Force -ErrorAction SilentlyContinue
 
+# Decompression
 if ($innerDirectory) {
     $tempExtract = Join-Path -Path $([System.IO.Path]::GetTempPath()) -ChildPath $((New-Guid).Guid)
     Expand-Archive -Path $pathZip -DestinationPath $tempExtract -Force
     Move-Item -Path "$tempExtract\*" -Destination $pathExtract -Force
-    #Move-Item -Path "$tempExtract\*\*" -Destination $location -Force
     Remove-Item -Path $tempExtract -Force -Recurse -ErrorAction SilentlyContinue
 }
 else {
     Expand-Archive -Path $pathZip -DestinationPath $pathExtract -Force
 }
 
+# Clean up
 Remove-Item $pathZip -Force
+
+# Symlink the file to the $elitePath
+New-Item -ItemType SymbolicLink -Path $(Join-Path -Path $elitePath -ChildPath "MinEdLauncher.exe") -Value (Join-Path -Path $pathExtract -ChildPath "MinEdLauncher.exe")
