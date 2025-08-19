2e54235bd3c829f6b28a881447df325c778dd830
gitkagehub <khgreenprime@gmail.com>
2025-08-18 21:07:19 -0700

    errorectomy

--- Companion Apps/edeb.ps1
+++ Companion Apps/edeb.ps1
@@ -1,5 +1,5 @@
 ### User Config ###
-$DestinationPath = 'G:\EliteApps'
+$DestinationPath = 'G:\EliteApps\'
 
 $websiteUrl = "https://www.panostrede.de/EDEB/"
 $htmlContent = Invoke-WebRequest -Uri $websiteUrl -UseBasicParsing -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
@@ -10,25 +10,20 @@ if (-not $msiLink) {
     throw "'.msi' not found."
 }
 else {
-    # If the link is relative, prepend base URL.
+    # Link is relative, prepend base
     $downloadUrl = $msiLink.href
     if ($downloadUrl -notmatch "https?://") {
         $uri = New-Object System.Uri($websiteUrl)
         $downloadUrl = New-Object System.Uri($uri, $msiLink.href).AbsoluteUri
     }
 
-    # Set destination path
-    if (-not $DestinationPath) {
-        # Default to the user's Downloads folder.
-        $DestinationPath = Join-Path -Path "$env:USERPROFILE" -ChildPath "Downloads"
-        # Ensure the directory exists.
-        if (-not (Test-Path $DestinationPath)) {
-            New-Item -ItemType Directory -Force -Path $DestinationPath | Out-Null
-        }
+    # Ensure the directory exists.
+    if (-not (Test-Path $DestinationPath)) {
+        New-Item -ItemType Directory -Force -Path $DestinationPath | Out-Null
     }
 
     # Extract the filename from the URL.
-    $fileName = [System.IO.Path]::GetFileName($downloadUrl)
+    $fileName = $msiLink.href
     $filePath = Join-Path -Path $DestinationPath -ChildPath $fileName
     Invoke-WebRequest -Uri $downloadUrl -OutFile $filePath
 }
