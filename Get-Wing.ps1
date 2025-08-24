c73f9782b203c7ffc377fc28e99a73c9a11586d6
gitkagehub <khgreenprime@gmail.com>
2025-08-23 20:41:03 -0700

    handlers and best practices

--- Get-Wing.ps1
+++ Get-Wing.ps1
@@ -1,5 +1,11 @@
 # Launch all four Elite Dangerous instances simultaneously.
-Start-Process -FilePath "C:\Users\Quadstronaut\scoop\apps\sandboxie-plus-np\current\Start.exe" -ArgumentList "/box:ED_Duvrazh", "C:\Program Files (x86)\Steam\steamapps\common\Elite Dangerous\MinEdLauncher.exe"
-Start-Process -FilePath "C:\Users\Quadstronaut\scoop\apps\sandboxie-plus-np\current\Start.exe" -ArgumentList "/box:ED_Bistronaut", "C:\Program Files (x86)\Steam\steamapps\common\Elite Dangerous\MinEdLauncher.exe"
-Start-Process -FilePath "C:\Users\Quadstronaut\scoop\apps\sandboxie-plus-np\current\Start.exe" -ArgumentList "/box:ED_Tristronaut", "C:\Program Files (x86)\Steam\steamapps\common\Elite Dangerous\MinEdLauncher.exe"
-Start-Process -FilePath "C:\Users\Quadstronaut\scoop\apps\sandboxie-plus-np\current\Start.exe" -ArgumentList "/box:ED_Quadstronaut", "C:\Program Files (x86)\Steam\steamapps\common\Elite Dangerous\MinEdLauncher.exe"
+$sandboxieStart = 'C:\Users\Quadstronaut\scoop\apps\sandboxie-plus-np\current\Start.exe'
+$edminlauncher = 'G:\SteamLibrary\steamapps\common\Elite Dangerous\MinEdLauncher.exe'
+$sbsTrue = Test-Path $edminlauncher
+$edmlTrue = Test-Path $edminlauncher
+if ($sbTrue -and $edmlTrue){
+    Start-Process -FilePath $sandboxieStart -ArgumentList "/box:CMDRDuvrazh `"$edminlauncher`" /frontier Account1 /edo /autorun /autoquit"
+    Start-Process -FilePath $sandboxieStart -ArgumentList "/box:CMDRBistronaut `"$edminlauncher`" /frontier Account2 /edo /autorun /autoquit"
+    Start-Process -FilePath $sandboxieStart -ArgumentList "/box:CMDRTristronaut `"$edminlauncher`" /frontier Account3 /edo /autorun /autoquit"
+    Start-Process -FilePath $sandboxieStart -ArgumentList "/box:CMDRQuadstronaut `"$edminlauncher`" /frontier Account4 /edo /autorun /autoquit"
+}
