3cc0c3a70248020841a7bc927f7cfe4f5ab64b8d
gitkagehub <khgreenprime@gmail.com>
2025-08-23 21:02:43 -0700

    corrections to tests

--- Get-Wing.ps1
+++ Get-Wing.ps1
@@ -1,11 +1,13 @@
-# Launch all four Elite Dangerous instances simultaneously.
+# Today's inflight entertainment brought to you by
 $sandboxieStart = 'C:\Users\Quadstronaut\scoop\apps\sandboxie-plus-np\current\Start.exe'
 $edminlauncher = 'G:\SteamLibrary\steamapps\common\Elite Dangerous\MinEdLauncher.exe'
-$sbsTrue = Test-Path $edminlauncher
+
+# Launch!
+$sbsTrue = Test-Path $sandboxieStart
 $edmlTrue = Test-Path $edminlauncher
-if ($sbTrue -and $edmlTrue){
-    Start-Process -FilePath $sandboxieStart -ArgumentList "/box:CMDRDuvrazh `"$edminlauncher`" /frontier Account1 /edo /autorun /autoquit"
-    Start-Process -FilePath $sandboxieStart -ArgumentList "/box:CMDRBistronaut `"$edminlauncher`" /frontier Account2 /edo /autorun /autoquit"
-    Start-Process -FilePath $sandboxieStart -ArgumentList "/box:CMDRTristronaut `"$edminlauncher`" /frontier Account3 /edo /autorun /autoquit"
-    Start-Process -FilePath $sandboxieStart -ArgumentList "/box:CMDRQuadstronaut `"$edminlauncher`" /frontier Account4 /edo /autorun /autoquit"
+if ($sbsTrue -and $edmlTrue){
+    Start-Process -FilePath $sandboxieStart -ArgumentList "/box:CMDRDuvrazh `"$edminlauncher`" /frontier Account1 /edo /autorun /autoquit /skipInstallPrompt"
+    Start-Process -FilePath $sandboxieStart -ArgumentList "/box:CMDRBistronaut `"$edminlauncher`" /frontier Account2 /edo /autorun /autoquit /skipInstallPrompt"
+    Start-Process -FilePath $sandboxieStart -ArgumentList "/box:CMDRTristronaut `"$edminlauncher`" /frontier Account3 /edo /autorun /autoquit /skipInstallPrompt"
+    Start-Process -FilePath $sandboxieStart -ArgumentList "/box:CMDRQuadstronaut `"$edminlauncher`" /frontier Account4 /edo /autorun /autoquit /skipInstallPrompt"
 }
