7e2c7b264a5a1a487ff347874e2cab0f9866b6ec
Quadstronaut <khgreenprime@gmail.com>
2025-09-20 20:58:16 -0700

    Update wrapper_rackham_wine.sh
    conform with foobar convention

--- companion_scripts/cron_files/wrapper_rackham_wine.sh
+++ companion_scripts/cron_files/wrapper_rackham_wine.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Define variables
-SCRIPT_DIR="/home/quadstronaut/cron_files/rackham_wine"
+SCRIPT_DIR="/foo/bar/cron_files/rackham_wine"
 PYTHON_SCRIPT="rackham_wine.py"
 LOG_FILE="crontab.log"
 PYTHON_INTERPRETER="/usr/bin/python3"
 
 # Set the home directory for the cron environment
-export HOME="/home/quadstronaut/cron_files/rackham_wine"
+export HOME="/foo/bar/cron_files/rackham_wine"
 
 # Navigate to the script's directory
 cd "$SCRIPT_DIR" || { echo "Failed to change directory to $SCRIPT_DIR" >&2; exit 1; }
