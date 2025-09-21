76da571ea5e3752322f9e6a90eacf53bcb3bfb52
gitkagehub <khgreenprime@gmail.com>
2025-09-20 20:48:35 -0700

    crontab bugfix

--- companion_scripts/cron_files/wrapper_rackham_wine.sh
+++ companion_scripts/cron_files/wrapper_rackham_wine.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Define variables for key directories and files
-SCRIPT_DIR="/home/foobar/cron_files/rackham_wine"
+# Define variables
+SCRIPT_DIR="/home/quadstronaut/cron_files/rackham_wine"
 PYTHON_SCRIPT="rackham_wine.py"
 LOG_FILE="crontab.log"
 PYTHON_INTERPRETER="/usr/bin/python3"
 
 # Set the home directory for the cron environment
-export HOME="/home/foobar"
+export HOME="/home/quadstronaut/cron_files/rackham_wine"
 
-# Navigate to the script's directory using the variable
+# Navigate to the script's directory
 cd "$SCRIPT_DIR" || { echo "Failed to change directory to $SCRIPT_DIR" >&2; exit 1; }
 
-# Run the Python script using the variables
-"$PYTHON_INTERPRETER" "$SCRIPT_DIR/$PYTHON_SCRIPT" >> "$SCRIPT_DIR/$LOG_FILE" 2>&1
+# Run the Python script with xvfb-run
+xvfb-run -a "$PYTHON_INTERPRETER" "$SCRIPT_DIR/$PYTHON_SCRIPT" >> "$SCRIPT_DIR/$LOG_FILE" 2>&1
